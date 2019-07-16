df7efd3d4eb271b09e35cde1053ae3f4622a0f57
haoxiaofeng91 <519773312@qq.com>
2019-07-16 23:44:45 +0800

    12

---
+++ ZJHTView.h
@@ -0,0 +1,16 @@
+//
+//  ZJHTView.h
+//  Pods-ZJHTestView_Tests
+//
+//  Created by haoxiaofeng on 2019/7/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJHTView : UIImageView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZJHTView.m
@@ -0,0 +1,12 @@
+//
+//  ZJHTView.m
+//  Pods-ZJHTestView_Tests
+//
+//  Created by haoxiaofeng on 2019/7/16.
+//
+
+#import "ZJHTView.h"
+
+@implementation ZJHTView
+
+@end
